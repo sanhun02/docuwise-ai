ac3f8e414bb12f002ea319615dc27973dd7cfb17
sanhun02 <sanyahhunter@gmail.com>
2024-06-20 02:53:10 -0500

    mock home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
 import NavBar from "@/components/NavBar";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
     return (
-        <>
+        <div className="relative h-screen w-screen flex flex-col">
             <NavBar />
-        </>
+            <div className="flex-grow flex flex-col items-center justify-center gap-10">
+                <div className="flex flex-col text-center gap-4">
+                    <h1 className="font-bold text-6xl">
+                        Transform PDFs with DocuWise AI
+                    </h1>
+                    <h3 className="font-medium text-2xl opacity-65">
+                        Quizzes, Flashcards, Highlights, and More
+                    </h3>
+                </div>
+
+                <Button className="bg-[#7E01FF] hover:bg-[#7E01FF]/85 text-lg p-6">
+                    Get Started For Free
+                </Button>
+            </div>
+        </div>
     );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,28 +1,33 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "@/components/ui/button"
 
 type Props = {};
 
 const NavBar = (props: Props) => {
     return (
-        <nav>
-            <div className="flex">
-                <div className="flex items-center gap-1 p-2">
-                    <Image
-                        src={"logo.svg"}
-                        width={40}
-                        height={40}
-                        alt="logo"
-                    ></Image>
-                    <h1 className="text-2xl font-medium">DocuWise AI</h1>
+        <nav className="flex py-5 px-20 justify-between">
+            <div className="flex gap-10 items-center">
+                <div>
+                    <Link href={"/"} className="flex items-center gap-1">
+                        <Image
+                            src={"logo.svg"}
+                            width={35}
+                            height={35}
+                            alt="logo"
+                        ></Image>
+                        <h1 className="text-2xl font-medium">DocuWise <span className="text-[#7E01FF]">AI</span></h1>
+                    </Link>
                 </div>
 
-                <div>
-                    <Link href={'/features'}>Features</Link>
-                    <Link href={'/pricing'}>Pricing</Link>
+                <div className="flex gap-8">
+                    <Link className="opacity-65 hover:opacity-100 hover:underline underline-offset-4" href={"/features"}>Features</Link>
+                    <Link className="opacity-65 hover:opacity-100 hover:underline underline-offset-4" href={"/pricing"}>Pricing</Link>
                 </div>
             </div>
+
+            <Button variant="outline" className="rounded-full text-base">Sign In</Button>
         </nav>
     );
 };
